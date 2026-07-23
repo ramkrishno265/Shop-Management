502020ba9dcac6f4e8e18e9ff88ac81ebef7da12
ramkrishno265 <ramkrishno265@gmail.com>
2026-07-23 17:00:44 +0600

    Update API URL to Render backend

--- backend/routes/salesRoutes.js
+++ backend/routes/salesRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { createSale } from '../controllers/salesController.js';
 // আপনার প্রজেক্টের অথেন্টিকেশন মিডলওয়্যার পাথ এখানে বসাবেন
-import { verifyToken } from '../middlewares/authMiddleware.js'; 
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // POST: /api/sales/create
-router.post('/create', verifyToken, createSale);
+router.post('/create', protect, createSale);
 
 export default router;
