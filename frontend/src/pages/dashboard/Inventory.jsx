d5cf62429abaa5121ee3a378e92f196c97686e4f
ramkrishno265 <ramkrishno265@gmail.com>
2026-07-23 12:55:15 +0600

    Add product edit and inventory improvements

--- frontend/src/pages/dashboard/Inventory.jsx
+++ frontend/src/pages/dashboard/Inventory.jsx
@@ -782,6 +782,7 @@ export default function Inventory() {
                     <option value="GM">Gram (GM)</option>
                     <option value="Liter">Liter</option>
                     <option value="Box">Box</option>
+                    <option value="Box">Bag</option>
 
                   </select>
                 </div>
