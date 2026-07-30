b3f02bf7261267edcdf0200f644b7075bff56d01
ramkrishno265 <ramkrishno265@gmail.com>
2026-07-30 15:02:32 +0600

    Update project

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { addExpense, getExpenses } from '../controllers/expenseController.js';
-import { verifyToken } from '../middlewares/authMiddleware.js'; // খেয়াল রাখবেন শেষে যেন .js থাকে
+import { verifyToken } from '../middleware/authMiddleware.js'; // খেয়াল রাখবেন শেষে যেন .js থাকে
 
 const router = express.Router();
 
